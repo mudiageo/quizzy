2c42d333a0734cc5c5bbadc78d08cc0219b3d092
Mudia Geo <mudiageo@gmail.com>
2025-06-29 15:57:57 +0000

    add pdf extractor script

--- api/app/index.py
+++ api/app/index.py
@@ -1,8 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, Response, jsonify
+from flask_cors import CORS
 from app.main import bp as main_bp
-
+from .pdfextractor import convert_pdf_to_txt
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3001"}})  # Svelte dev server
 app.register_blueprint(main_bp, url_prefix='/main')
 
 
---
+++ api/app/main/pdfextractor.py
@@ -0,0 +1,34 @@
+import io
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+
+# "C:\Users\WALTER\Downloads\GOT Senior Syllabus.pdf"
+def convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    codec = 'utf-8'
+    laparams = LAParams()
+
+    with io.StringIO() as retstr:
+        with TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams) as device:
+            with open(path, 'rb') as fp:
+                interpreter = PDFPageInterpreter(rsrcmgr, device)
+                password = ""
+                maxpages = 0
+                caching = True
+                pagenos = set()
+
+                for page in PDFPage.get_pages(fp,
+                                              pagenos,
+                                              maxpages=maxpages,
+                                              password=password,
+                                              caching=caching,
+                                              check_extractable=True):
+                    interpreter.process_page(page)
+                return retstr.getvalue()
+
+
+if __name__ == '__main__':
+    print(convert_pdf_to_txt(r"C:\Users\WALTER\Downloads\GOT Senior Syllabus.pdf"))
